8633de9d78380a35b1cfe3db1705f0b08134c8ce
Daniel Ors <do7811@gmail.com>
2019-04-12 09:52:04 -0400

    added basic get and delete requests

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const { dogInfo, catInfo } = require('./store');
+const PORT = require('./config');
 
 const app = express();
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
 app.use(cors());
 
 // Catch-all 404
@@ -12,23 +20,39 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-app.use('/api/cat', function (req, res, next) {
-  req.app.get()
-    .then(res => {
-      if (res.ok) {
-        return res.json({
-          imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-          imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-          name: 'Fluffy',
-          sex: 'Female',
-          age: 2,
-          breed: 'Bengal',
-          story: 'Thrown on the street'
-        });
-      }
-    })
-    .catch(next);
+app.get('/api/cat', function (req, res, next) {
+  res.json(catInfo[0])
+    .catch(err => {
+      next(err);
+    });
 
+});
+
+app.get('/api/dog', (req, res, next) => {
+  res.json(dogInfo[0])
+    .catch(err => {
+      next(err);
+    });
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  Promise.all([
+    catInfo.shift(),
+    res.sendStatus(204)
+      .catch(err => {
+        next(err);
+      })
+  ]);
+});
+
+app.delete('/api/dog', (req, res, next) => {
+  Promise.all([
+    dogInfo.shift(),
+    res.sendStatus(204)
+      .catch(err => {
+        next(err);
+      })
+  ]);
 });
 
 // Catch-all Error handler
@@ -41,6 +65,6 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(8080,()=>{
-  console.log('Serving on 8080');
+app.listen(PORT,()=>{
+  console.log(`Server is listening on port ${PORT}`);
 });
