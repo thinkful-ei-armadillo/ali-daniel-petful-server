aa22d429e16343f62ecb3586b7525693e7236dd9
Ali Lahrime <alahrime@email.wm.edu>
2019-04-12 17:29:54 -0400

    progress on delete endpoint sending back user

--- server.js
+++ server.js
@@ -1,59 +1,82 @@
-'use strict';
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const { catRefill, dogRefill, Cats, Dogs } = require('./src/store');
-const {PORT, NODE_ENV, CLIENT_ORIGIN} = require('./src/config');
+"use strict";
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const {
+  catRefill,
+  dogRefill,
+  userRefill,
+  Cats,
+  Dogs,
+  Users
+} = require("./src/store");
+const { PORT, NODE_ENV, CLIENT_ORIGIN } = require("./src/config");
 
 const app = express();
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}));
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test"
+  })
+);
 app.use(helmet());
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
+// app.get("/", function(req, res, next) {
+//   res.json(console.log("Hello World")).catch(next);
+// });
 
-app.get('/', function (req, res, next) {
-  res.json(console.log('Hello World'))
-    .catch(next);
-});
-
-app.get('/api/cat', function (req, res, next) {
+app.get("/api/cat", function(req, res, next) {
   const cat = Cats.peek();
   if (!cat) {
     catRefill();
     const newCat = Cats.peek();
     return res.send(newCat);
   }
-  return res.send(cat)
-    .catch(next);
+  return res.send(cat); //.catch(next);
 });
 
-app.get('/api/dog', (req, res, next) => {
+app.get("/api/dog", (req, res, next) => {
   const dog = Dogs.peek();
   if (!dog) {
     dogRefill();
     const newDog = Dogs.peek();
     return res.send(newDog);
-  } 
-  else
-    return res.send(dog)
-      .catch(next);
+  } else return res.send(dog); //.catch(next);
 });
 
-app.delete('/api/cat', (req, res, next) => {
+// app.get("/api/user", (req, res, next) => {
+//   const user = Users.peek();
+//   if (!user) {
+//     userRefill();
+//     const newUser = Users.peek();
+//     return res.send(newUser);
+//   } else return res.send(user).catch(next);
+// });
+
+app.delete("/api/cat", (req, res, next) => {
   Cats.dequeue();
-  res.status(204).send();
+  const user = Users.peek();
+  if (!user) {
+    userRefill();
+  }
+  const adopter = Users.dequeue();
+  res.status(200).json({ name: adopter.name });
 });
 
-app.delete('/api/dog', (req, res, next) => {
+app.delete("/api/dog", (req, res, next) => {
   Dogs.dequeue();
-  res.status(204).send();
+  const user = Users.peek();
+  if (!user) {
+    userRefill();
+  }
+  const adopter = Users.dequeue();
+  res.status(200).json({ name: adopter.name });
 });
-
 
 // // Catch-all 404
 // app.use(function (req, res, next) {
@@ -62,19 +85,16 @@ app.delete('/api/dog', (req, res, next) => {
 //   next(err);
 // });
 
-
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'production') {
-    response = { error: 'Server error' };
+  if (NODE_ENV === "production") {
+    response = { error: "Server error" };
   } else {
     console.error(error);
     response = { error: error.message, object: error };
   }
   res.status(500).json(response);
 });
-
-
 
 // // Catch-all Error handler
 // // Add NODE_ENV check to prevent stacktrace leak
@@ -86,6 +106,6 @@ app.use(function errorHandler(error, req, res, next) {
 //   });
 // });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
--- src/store.js
+++ src/store.js
@@ -1,89 +1,95 @@
-'use strict';
+"use strict";
 // data for JSON responses
 
 const dogInfo = [
   {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
+    imageURL:
+      "http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg",
+    imageDescription:
+      "A smiling golden-brown golden retreiver listening to music.",
+    name: "Zeus",
+    sex: "Male",
     age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
+    breed: "Golden Retriever",
+    story: "Owner Passed away"
   },
   {
-    imageURL: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/istock_86999965_small.jpg?itok=EzyZsGnK&resize=1100x1100',
-    imageDescription: 'A loveable Pitbull',
-    name: 'Beans',
-    sex: 'Female',
+    imageURL:
+      "https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/istock_86999965_small.jpg?itok=EzyZsGnK&resize=1100x1100",
+    imageDescription: "A loveable Pitbull",
+    name: "Beans",
+    sex: "Female",
     age: 7,
-    breed: 'Pitbull',
-    story: 'A sordid past riddled with intrigue.'
+    breed: "Pitbull",
+    story: "A sordid past riddled with intrigue."
   },
   {
-    imageURL: 'https://vetstreet.brightspotcdn.com/dims4/default/514331d/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F26%2F0d%2F6443c92b4c67ae315a8bb8be16e1%2FChihuahua-AP-KIDP62-645lc061113.jpg',
-    imageDescription: 'A regal chihuahua',
-    name: 'Cous Cous',
-    sex: 'Male',
+    imageURL:
+      "https://vetstreet.brightspotcdn.com/dims4/default/514331d/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F26%2F0d%2F6443c92b4c67ae315a8bb8be16e1%2FChihuahua-AP-KIDP62-645lc061113.jpg",
+    imageDescription: "A regal chihuahua",
+    name: "Cous Cous",
+    sex: "Male",
     age: 2,
-    breed: 'Chihuahua',
-    story: 'Owner passed away, couldn\'t handle cuteness of this dog.'
+    breed: "Chihuahua",
+    story: "Owner passed away, couldn't handle cuteness of this dog."
   }
 ];
 
 const catInfo = [
   {
-    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
+    imageURL:
+      "https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg",
+    imageDescription:
+      "Orange bengal cat with black stripes lounging on concrete.",
+    name: "Fluffy",
+    sex: "Female",
     age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
+    breed: "Bengal",
+    story: "Thrown on the street"
   },
   {
-    imageURL: 'https://www.thesprucepets.com/thmb/0Y_9qW07-uYqkW9_kcasnwXqCi0=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/twenty20_d4afe7d2-ebe8-4288-a2ef-bcecbb99df88-5a8c4309c064710037e9965e.jpg',
-    imageDescription: 'Adorable kitten that loves balls',
-    name: 'Roger',
-    sex: 'Male',
+    imageURL:
+      "https://www.thesprucepets.com/thmb/0Y_9qW07-uYqkW9_kcasnwXqCi0=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/twenty20_d4afe7d2-ebe8-4288-a2ef-bcecbb99df88-5a8c4309c064710037e9965e.jpg",
+    imageDescription: "Adorable kitten that loves balls",
+    name: "Roger",
+    sex: "Male",
     age: 1,
-    breed: 'Scottish Fold',
-    story: 'Left alone behind a trailer'
+    breed: "Scottish Fold",
+    story: "Left alone behind a trailer"
   },
   {
-    imageURL: 'https://vetstreet.brightspotcdn.com/dims4/default/167768e/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F0d%2F4b1f209dc511e0a2380050568d634f%2Ffile%2FAbyssinian-3-645mk062211.jpg',
-    imageDescription: 'Adorable kitten -- adopt her please!',
-    name: 'Sphynx',
-    sex: 'Female',
+    imageURL:
+      "https://vetstreet.brightspotcdn.com/dims4/default/167768e/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F0d%2F4b1f209dc511e0a2380050568d634f%2Ffile%2FAbyssinian-3-645mk062211.jpg",
+    imageDescription: "Adorable kitten -- adopt her please!",
+    name: "Sphynx",
+    sex: "Female",
     age: 4,
-    breed: 'Russian',
-    story: 'Loves hacking into servers in its free time.'
+    breed: "Russian",
+    story: "Loves hacking into servers in its free time."
   }
 ];
 
 const userInfo = [
   {
-    name: 'Tauhida'
+    name: "Tauhida"
   },
   {
-    name: 'Alex'
+    name: "Alex"
   },
   {
-    name: 'Wenceslao'
+    name: "Wenceslao"
   },
   {
-    name: 'Daniel'
+    name: "Daniel"
   },
   {
-    name: 'Ali'
+    name: "Ali"
   }
 ];
 
 class _Node {
   constructor(value) {
-    this.value = value,
-    this.next = null,
-    this.prev = null;
+    (this.value = value), (this.next = null), (this.prev = null);
   }
 }
 class Queue {
@@ -116,8 +122,7 @@ class Queue {
   }
 
   peek() {
-    if (this.first)
-      return this.first.value;
+    if (this.first) return this.first.value;
     else return null;
   }
 }
@@ -138,7 +143,6 @@ Users.enqueue(userInfo[1]);
 Users.enqueue(userInfo[2]);
 Users.enqueue(userInfo[3]);
 
-
 function catRefill() {
   Cats.enqueue(catInfo[1]);
   Cats.enqueue(catInfo[2]);
@@ -151,11 +155,20 @@ function dogRefill() {
   Dogs.enqueue(dogInfo[1]);
 }
 
+function userRefill() {
+  Users.enqueue(UserInfo[0]);
+  Users.enqueue(userInfo[1]);
+  Users.enqueue(userInfo[2]);
+  Users.enqueue(userInfo[3]);
+}
+
 module.exports = {
-  dogInfo, 
+  dogInfo,
   catInfo,
-  catRefill, 
+  catRefill,
   dogRefill,
+  userRefill,
   Cats,
-  Dogs
+  Dogs,
+  Users
 };
